d70c65b2ef7d42aa27e231f7d50d144c415549b7
Sarah Larson <sarahjlarson21@gmail.com>
2019-05-23 15:06:18 -0600

    update with link to website

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-window.onload = function() {
-    document.getElementById("bookSubmit").addEventListener("click", async function(event) {
-        event.preventDefault();
-        const value = document.getElementById("bookInput").value;
-        if (value === "")
-            return;
-        console.log(value);
-    });
-}
+// window.onload = function() {
+//     document.getElementById("bookSubmit").addEventListener("click", async function(event) {
+//         event.preventDefault();
+//         const value = document.getElementById("bookInput").value;
+//         if (value === "")
+//             return;
+//         console.log(value);
+//     });
+// }
 
 let app= new Vue({
     el: '#app',
     data: {
-        loading: true,
+        loading: false,
         current: {
             //if no info- don't display
             title: '',
@@ -25,21 +25,40 @@ let app= new Vue({
     },
     methods: {
         async quickbook() {
+            //window.onload = function() {
+            //     document.getElementById("bookSubmit").addEventListener("click", async function(event) {
+            //         event.preventDefault();
+                    
+            //         if (value === "")
+            //             return;
+            //         console.log(value);
+            //     });
+            // const url1="https://openlibrary.org/search.json?q=" + value;
             try {
+                // const response1 = await fetch(url1);
+                // console.log("response",response1);
+                // const json = await response1.json();
                 this.loading = true;
                 const value = document.getElementById("bookInput").value;
-                if (value === "")
-                    return;
-                console.log(value);
-                const response = await axios.get('https://openlibrary.org/search.json?q=' + value);
+                const response = await axios.get('https://openlibrary.org/search.json?q=' + this.value);
                 this.current = response.data;
                 this.loading = false;
+                //this.value = response.data.docs;
+                let results ="";
+                results += current.docs[0].title;
+                
+                document.getElementById("bookResults").innerHTML = results;
             } catch(error) {
                 console.log(error);
             }
         },
+        favorite() {
+            this.books.push();
+        },
     },
     computed: {
-
+        bookResults() {
+            //return this.books.docs[i];
+        }
     }
 });
